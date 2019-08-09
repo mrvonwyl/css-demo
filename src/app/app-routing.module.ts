b4a7c9f9abaa19ff1c9d0eeac18da48ade037e15
Marcel von Wyl <mrvonwyl@gmail.com>
2019-08-09 09:21:32 +0200

    nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { PositionComponent } from "./position/position.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { DisplayComponent } from "./display/display.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "position", component: PositionComponent },
+  { path: "display", component: DisplayComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PositionComponent } from './position/position.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { DisplayComponent } from './display/display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PositionComponent,
+    NavigationComponent,
+    DisplayComponent
   ],
   imports: [
     BrowserModule,
